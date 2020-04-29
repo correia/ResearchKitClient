1f9e12176b21fbc5e81114db9d33d4775f448e9d
Jim Correia <jim.correia@pobox.com>
2020-04-29 10:02:50 -0700

    Initial Commit

---
+++ ResearchKitClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResearchKitClient
+//
+//  Created by Jim Correia on 4/29/20.
+//  Copyright © 2020 Jim Correia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
